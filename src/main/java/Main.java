76181c5bc3eb74d0d0206363377a0d87aaa6d56a
tomaszlach86 <tomaszlach86@gmail.com>
2019-05-21 05:40:53 +0200

    improved styling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Tablica tab = new Tablica(new int[5]);
-       tab.fillTab();
+        tab.fillTab();
         System.out.println(tab);
         System.out.println(tab.sumOddElements());
 
