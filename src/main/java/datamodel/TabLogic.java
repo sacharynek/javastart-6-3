c98b2f2b033db65a1cb11acbaa2ebb28ae6378c9
tomaszlach86 <tomaszlach86@gmail.com>
2019-05-21 18:15:24 +0200

    improved styling

--- src/main/java/datamodel/TabLogic.java
+++ src/main/java/datamodel/TabLogic.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class TabLogic {
     static Scanner sc = new Scanner(System.in);
+
     public static int[] fillTab(int[] tab) {
 
         for (int i = 0; i < tab.length; i++) {
